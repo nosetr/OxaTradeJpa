3531147a21d19acf595f07c7ce61c999f06a9ad8
nosetr <osetrov.n@gmail.com>
2024-02-09 18:15:09 +0100

    modules back-auth and back-library created

---
+++ back-auth/src/main/java/com/nosetr/auth/BackAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nosetr.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAuthApplication.class, args);
+	}
+
+}
---
+++ back-library/src/test/java/com/nosetr/library/BackLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosetr.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
