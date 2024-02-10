b3fe7261966d4409726e315ae3e84ae4f925b45a
nosetr <osetrov.n@gmail.com>
2024-02-10 08:06:51 +0100

    switch from R2DBC to Hibernate

--- back-auth/src/main/java/com/nosetr/auth/BackAuthApplication.java
+++ back-auth/src/main/java/com/nosetr/auth/BackAuthApplication.java
@@ -2,10 +2,12 @@ package com.nosetr.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "APIs", version = "1.0", description = "Documentation OxaAuth APIs v1.0"))
 public class BackAuthApplication {
